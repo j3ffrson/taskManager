089c7a5d5455747fd0aed3700e38c6d9f3fc039e
jeffer <chaustrejefferson@gmail.com>
2026-04-14 16:29:45 -0500

    feat: create entity task #4

---
+++ src/main/java/cj/projects/taskmanager/persistence/entities/TaskEntity.java
@@ -0,0 +1,30 @@
+package cj.projects.taskmanager.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "task")
+@Entity
+public class TaskEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+
+    private String title;
+    private String description;
+    private LocalDateTime createAd;
+    private LocalDateTime updateAd;
+    private LocalDateTime deleteAd;
+
+
+
+}
