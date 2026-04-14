00d274a3754c9d6823ed83ec49a1a5823957a685
jeffer <chaustrejefferson@gmail.com>
2026-04-14 17:12:09 -0500

    feat: create query method in task repository for find task by author #8

--- src/main/java/cj/projects/taskmanager/persistence/repositories/TaskRepository.java
+++ src/main/java/cj/projects/taskmanager/persistence/repositories/TaskRepository.java
@@ -5,11 +5,11 @@ import cj.projects.taskmanager.persistence.entities.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, UUID> {
     List<TaskEntity> findTaskEntitiesByStatus(Status status);
+    List<TaskEntity> findTaskEntitiesByAuthorUuid(UUID authorUuid);
 }
