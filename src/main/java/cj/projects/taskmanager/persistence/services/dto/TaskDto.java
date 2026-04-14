62e0ee204a1f8751907817250a771454111b0118
jeffer <chaustrejefferson@gmail.com>
2026-04-14 18:02:14 -0500

    fix(TaskDto): Changing the data type for dates

--- src/main/java/cj/projects/taskmanager/persistence/services/dto/TaskDto.java
+++ src/main/java/cj/projects/taskmanager/persistence/services/dto/TaskDto.java
@@ -1,10 +1,8 @@
 package cj.projects.taskmanager.persistence.services.dto;
 
-import cj.projects.taskmanager.persistence.entities.UserEntity;
 import cj.projects.taskmanager.persistence.entities.enums.Status;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -13,12 +11,12 @@ public class TaskDto {
     private UUID uuid;
     private String title;
     private String description;
-    private LocalDateTime createAd;
-    private LocalDateTime updateAd;
-    private LocalDateTime deleteAd;
+    private String createAd;
+    private String updateAd;
+    private String deleteAd;
 
     private Status status;
 
-    private UserEntity author;
+    private UserDto author;
 
 }
