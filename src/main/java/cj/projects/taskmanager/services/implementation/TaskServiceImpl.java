c781c7cb5ed4c93b2046934e25aedc8ddd048726
jeffer <chaustrejefferson@gmail.com>
2026-04-15 10:33:18 -0500

    feat(TaskServiceImpl): inject dependence from repository task #10

--- src/main/java/cj/projects/taskmanager/services/implementation/TaskServiceImpl.java
+++ src/main/java/cj/projects/taskmanager/services/implementation/TaskServiceImpl.java
@@ -1,16 +1,23 @@
 package cj.projects.taskmanager.services.implementation;
 
+import cj.projects.taskmanager.persistence.repositories.TaskRepository;
 import cj.projects.taskmanager.services.TaskService;
 import cj.projects.taskmanager.services.dto.request.TaskRequest;
 import cj.projects.taskmanager.services.dto.response.TaskDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 
+@RequiredArgsConstructor
+@Service
 public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
 
     @Override
     public Page<TaskDto> findAllTaskPage(Pageable pageable) {
