4d90080a8b1297b5d69edc10234f75e5beb436c4
jeffer <chaustrejefferson@gmail.com>
2026-04-20 09:05:11 -0500

    feat(TaskController): inject dependence from service task #24

--- src/main/java/cj/projects/taskmanager/controller/TaskController.java
+++ src/main/java/cj/projects/taskmanager/controller/TaskController.java
@@ -1,9 +1,15 @@
 package cj.projects.taskmanager.controller;
 
+import cj.projects.taskmanager.services.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "/api/task",version = "1.0")
 public class TaskController {
+
+    private final TaskService taskService;
+
 }
