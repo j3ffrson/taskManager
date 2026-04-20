4ac0d1483b0d7f47e8e9459d977ce6d427c46fee
jeffer <chaustrejefferson@gmail.com>
2026-04-20 09:01:03 -0500

    feat: Create controller for task #24

---
+++ src/main/java/cj/projects/taskmanager/controller/TaskController.java
@@ -0,0 +1,9 @@
+package cj.projects.taskmanager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/task",version = "1.0")
+public class TaskController {
+}
