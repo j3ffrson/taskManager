ebe92ca019cceebbf9954528657ee9c7c8db1a34
jeffer <chaustrejefferson@gmail.com>
2026-04-21 16:01:26 -0500

    feat: implement interface from security UserDetailService for operations security #32

---
+++ src/main/java/cj/projects/taskmanager/services/implementation/UserDetailsServiceImpl.java
@@ -0,0 +1,17 @@
+package cj.projects.taskmanager.services.implementation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
