8a2afa8f698f347d66e48de2e5973d6d560f32a1
jeffer <chaustrejefferson@gmail.com>
2026-04-21 16:16:47 -0500

    feat(UserSecurity): implement method from load user by username #32

--- src/main/java/cj/projects/taskmanager/services/implementation/UserDetailsServiceImpl.java
+++ src/main/java/cj/projects/taskmanager/services/implementation/UserDetailsServiceImpl.java
@@ -1,11 +1,17 @@
 package cj.projects.taskmanager.services.implementation;
 
+import cj.projects.taskmanager.persistence.entities.UserEntity;
 import cj.projects.taskmanager.persistence.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +22,18 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        UserEntity user= userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException(username));
+        List<SimpleGrantedAuthority> authorities= new ArrayList<>();
+
+        user.getRoles().forEach(roles->{
+            authorities.add(new SimpleGrantedAuthority(roles.getName().name()));
+        });
+
+        user.getRoles().stream()
+                .flatMap(role->role.getListaPermisos().stream())
+                .forEach(permission->authorities.add(new SimpleGrantedAuthority(permission.getName())));
+
+        return new User(user.getUsername(),user.getPassword(),authorities);
     }
 }
