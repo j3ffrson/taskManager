a1ea5b54046d679622f898bb2bb1c2b5837ea4c9
jeffer <chaustrejefferson@gmail.com>
2026-04-20 19:21:38 -0500

    chore(SecurityConfig): method for security Authentication management

--- src/main/java/cj/projects/taskmanager/configurations/SecurityConfig.java
+++ src/main/java/cj/projects/taskmanager/configurations/SecurityConfig.java
@@ -2,7 +2,9 @@ package cj.projects.taskmanager.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +25,11 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration){
+        return configuration.getAuthenticationManager();
     }
 
 }
