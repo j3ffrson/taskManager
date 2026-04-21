691629e57fdf7bbc4dc2ce02dcc72db107d4fab8
jeffer <chaustrejefferson@gmail.com>
2026-04-21 16:06:11 -0500

    feat(UserSecurity): Inject repository from user #32

--- src/main/java/cj/projects/taskmanager/services/implementation/UserDetailsServiceImpl.java
+++ src/main/java/cj/projects/taskmanager/services/implementation/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package cj.projects.taskmanager.services.implementation;
 
+import cj.projects.taskmanager.persistence.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
