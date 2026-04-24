7f87d5ecc80b46b29b02e85053b429e67a6add8f
jeffer <chaustrejefferson@gmail.com>
2026-04-24 16:57:30 -0500

    feat: create test from TaskServiceImpl

---
+++ src/test/java/cj/projects/taskmanager/services/implementation/TaskServiceImplTest.java
@@ -0,0 +1,40 @@
+package cj.projects.taskmanager.services.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceImplTest {
+
+    @Test
+    void findAllTaskPageTest() {
+    }
+
+    @Test
+    void findAllTaskByStatusPageTest() {
+    }
+
+    @Test
+    void findAllTaskByAuthorPageTest() {
+    }
+
+    @Test
+    void findAllTaskByCreateAdDateBetweenTest() {
+    }
+
+    @Test
+    void findTaskByIdTest() {
+    }
+
+    @Test
+    void createNewTaskTest() {
+    }
+
+    @Test
+    void updateNewTaskTest() {
+    }
+
+    @Test
+    void deleteTaskByIdTest() {
+    }
+}
