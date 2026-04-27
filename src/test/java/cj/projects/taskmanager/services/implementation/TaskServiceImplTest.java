7359dff0ebe899f00b57142c8522b635defa0fa8
jeffer <chaustrejefferson@gmail.com>
2026-04-27 15:25:13 -0500

    test(TaskServiceImpl): create test for method get list tasks #37

--- src/test/java/cj/projects/taskmanager/services/implementation/TaskServiceImplTest.java
+++ src/test/java/cj/projects/taskmanager/services/implementation/TaskServiceImplTest.java
@@ -1,14 +1,24 @@
 package cj.projects.taskmanager.services.implementation;
 
+import cj.projects.taskmanager.persistence.entities.TaskEntity;
 import cj.projects.taskmanager.persistence.repositories.TaskRepository;
 import cj.projects.taskmanager.persistence.repositories.UserRepository;
+import cj.projects.taskmanager.services.dto.response.TaskDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
+import static cj.projects.taskmanager.DataProvider.TaskDataProvider.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceImplTest {
@@ -21,8 +31,26 @@ class TaskServiceImplTest {
     @InjectMocks
     private TaskServiceImpl taskService;
 
+
     @Test
     void findAllTaskPageTest() {
+
+        Pageable pageable= PageRequest.of(1,3);
+        List<TaskEntity> taskList= List.of(getTaskEntity1(),getTaskEntity2(),getTaskEntity3());
+        List<TaskDto> taskDtoList= List.of(getTaskDto1(),getTaskDto2(),getTaskDto3());
+        Page<TaskEntity> entityPage= new PageImpl<>(taskList,pageable,3);
+
+        when(taskRepository.findAll(pageable)).thenReturn(entityPage);
+
+        Page<TaskDto> dtoPage= taskService.findAllTaskPage(pageable);
+
+        assertThat(dtoPage.getContent()).isEqualTo(taskDtoList);
+        assertThat(dtoPage.getContent().size()).isEqualTo(taskDtoList.size());
+        assertThat(dtoPage.getContent().getFirst().getTitle()).isEqualTo(taskDtoList.getFirst().getTitle());
+        assertThat(dtoPage.getContent().get(1).getTitle()).isEqualTo(taskDtoList.get(1).getTitle());
+        assertThat(dtoPage.getContent().getLast().getTitle()).isEqualTo(taskDtoList.getLast().getTitle());
+        assertThat(dtoPage.getContent().getFirst().getId()).isEqualTo(taskDtoList.getFirst().getId());
+
     }
 
     @Test
