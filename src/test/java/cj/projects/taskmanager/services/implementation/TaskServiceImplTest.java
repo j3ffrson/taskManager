b5db9a787678a1ca3e123db48b6329de2af31a23
jeffer <chaustrejefferson@gmail.com>
2026-04-27 10:45:20 -0500

    chore: add Mockito class extension in the test

--- src/test/java/cj/projects/taskmanager/services/implementation/TaskServiceImplTest.java
+++ src/test/java/cj/projects/taskmanager/services/implementation/TaskServiceImplTest.java
@@ -1,10 +1,25 @@
 package cj.projects.taskmanager.services.implementation;
 
+import cj.projects.taskmanager.persistence.repositories.TaskRepository;
+import cj.projects.taskmanager.persistence.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class TaskServiceImplTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private TaskServiceImpl taskService;
 
     @Test
     void findAllTaskPageTest() {
