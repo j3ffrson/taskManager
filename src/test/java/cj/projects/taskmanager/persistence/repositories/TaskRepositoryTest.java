e0ec3e660299cd5f46ae4f900bbc268ae5b9cdc2
jeffer <chaustrejefferson@gmail.com>
2026-04-28 14:51:10 -0500

    test: Create test for repository methods #42

---
+++ src/test/java/cj/projects/taskmanager/persistence/repositories/TaskRepositoryTest.java
@@ -0,0 +1,43 @@
+package cj.projects.taskmanager.persistence.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskRepositoryTest {
+
+    @Test
+    void findAllTaskTest(){
+    }
+
+    @Test
+    void findTaskEntitiesById() {
+    }
+
+    @Test
+    void findTaskEntitiesByStatus() {
+    }
+
+    @Test
+    void findTaskEntitiesByAuthor() {
+    }
+
+    @Test
+    void findTaskEntitiesByCreateAdBetween() {
+    }
+
+    @Test
+    void saveTaskTest(){
+    }
+
+    @Test
+    void updateTaskTestById(){
+    }
+
+    @Test
+    void deleteTaskTestById(){
+    }
+
+
+
+}
