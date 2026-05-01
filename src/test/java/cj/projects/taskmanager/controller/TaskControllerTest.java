fadc54071637417bbb028f9a7ade83c2d2b911ae
jeffer <chaustrejefferson@gmail.com>
2026-05-01 11:19:20 -0500

    test(TaskControllerTest): create method for search list tasks and test using a rest client #47

--- src/test/java/cj/projects/taskmanager/controller/TaskControllerTest.java
+++ src/test/java/cj/projects/taskmanager/controller/TaskControllerTest.java
@@ -68,6 +68,27 @@ class TaskControllerTest {
 
     @Test
     void getAllTaskTest() {
+
+        TaskDto taskDto1= getTaskDto1();
+        TaskDto taskDto2= getTaskDto2();
+        TaskDto taskDto3= getTaskDto3();
+
+        taskDto1.setId(task1.getId());
+        taskDto2.setId(task2.getId());
+        taskDto3.setId(task3.getId());
+
+        List<TaskDto> dtoList=List.of(taskDto1,taskDto2,taskDto3);
+        restTestClient.get().uri("/api/tasks").header("Api-Version", "1")
+                .exchange()
+                .expectAll(
+                        spect -> spect.expectHeader().contentType(MediaType.APPLICATION_JSON),
+                        spect -> spect.expectStatus().isOk(),
+                        spect -> spect.expectBody(Map.class),
+                        spect -> spect.expectBody().jsonPath("$.content").isArray(),
+                        spect -> spect.expectBody().jsonPath("$.page.number").isEqualTo(0),
+                        spect -> spect.expectBody().jsonPath("$.page.totalElements").isEqualTo(3),
+                        spect -> spect.expectBody().jsonPath("$.page.size").isEqualTo(3)
+                );
     }
 
     @Test
