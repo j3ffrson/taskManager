c88cee971a94c9bd19b7f134e1988baae47517cd
jeffer <chaustrejefferson@gmail.com>
2026-04-30 19:34:15 -0500

    test(TaskControllerTest): Create test for TaskController

---
+++ src/test/java/cj/projects/taskmanager/controller/TaskControllerTest.java
@@ -0,0 +1,45 @@
+package cj.projects.taskmanager.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllTaskTest() {
+    }
+
+    @Test
+    void getAllTaskByStatusTest() {
+    }
+
+    @Test
+    void getAllTaskByAuthorTest() {
+    }
+
+    @Test
+    void getAllTaskByPeriodTimeTest() {
+    }
+
+    @Test
+    void getTaskByIdTest() {
+    }
+
+    @Test
+    void createNewTaskTest() {
+    }
+
+    @Test
+    void updateTaskTest() {
+    }
+
+    @Test
+    void deleteTaskTest() {
+    }
+}
