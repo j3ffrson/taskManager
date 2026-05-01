ec27a060952499d83cfc5406aca8af26f22a3e87
jeffer <chaustrejefferson@gmail.com>
2026-05-01 09:39:23 -0500

    test(TaskControllerTest): add testcontainer configs for controller #47

--- src/test/java/cj/projects/taskmanager/controller/TaskControllerTest.java
+++ src/test/java/cj/projects/taskmanager/controller/TaskControllerTest.java
@@ -2,14 +2,29 @@ package cj.projects.taskmanager.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.web.servlet.client.RestTestClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Testcontainers
+@AutoConfigureRestTestClient
 class TaskControllerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Container
+    @ServiceConnection
+    private static PostgreSQLContainer postgreSQLContainer=
+            new PostgreSQLContainer("postgres:latest");
+
+    @Autowired
+    private RestTestClient restTestClient;
+
 
     @Test
     void getAllTaskTest() {
