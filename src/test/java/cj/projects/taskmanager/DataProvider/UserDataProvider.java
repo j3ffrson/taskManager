31493155d04cb653afc2e707811598afc7e0a536
jeffer <chaustrejefferson@gmail.com>
2026-05-03 13:30:13 -0500

    refactor(UserDataProvider): Refactor the user provider so that it returns a new user instance. Ensure that tests do not share the user object.

--- src/test/java/cj/projects/taskmanager/DataProvider/UserDataProvider.java
+++ src/test/java/cj/projects/taskmanager/DataProvider/UserDataProvider.java
@@ -6,64 +6,40 @@ import cj.projects.taskmanager.persistence.entities.UserEntity;
 import cj.projects.taskmanager.persistence.entities.enums.Roles;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public class UserDataProvider {
 
-    private static PermissionEntity create;
-    private static PermissionEntity read;
-    private static PermissionEntity update;
-    private static PermissionEntity delete;
-    private static RoleEntity admin;
-    private static UserEntity user;
+    public static UserEntity getUser() {
+        PermissionEntity create = PermissionEntity.builder().name("CREATE").build();
+        PermissionEntity read = PermissionEntity.builder().name("READ").build();
+        PermissionEntity update = PermissionEntity.builder().name("UPDATE").build();
+        PermissionEntity delete = PermissionEntity.builder().name("DELETE").build();
 
-    static{
-        create= PermissionEntity.builder().name("CREATE").build();
-        read= PermissionEntity.builder().name("READ").build();
-        update= PermissionEntity.builder().name("UPDATE").build();
-        delete= PermissionEntity.builder().name("DELETE").build();
+        Set<PermissionEntity> permissions = new HashSet<>();
+        permissions.add(create);
+        permissions.add(read);
+        permissions.add(update);
+        permissions.add(delete);
 
-        admin= RoleEntity.builder()
+        RoleEntity adminRole = RoleEntity.builder()
                 .name(Roles.USER)
-                .listaPermisos(Set.of(create,read,update,delete))
+                .listaPermisos(permissions)
                 .build();
 
-        user= UserEntity.builder()
+        return UserEntity.builder()
                 .name("Jefferson")
                 .lastName("Chaustre")
                 .username("jeffer")
                 .email("chaustrejefferson@gmail.com")
                 .password("passtest")
-                .roles(Set.of(admin))
+                .roles(Set.of(adminRole))
                 .tasks(Collections.emptyList())
                 .isEnabled(true)
                 .isAccountNonExpired(true)
                 .isCredentialsNonExpired(true)
                 .isAccountNonLocked(true)
                 .build();
-    }
-
-    public static PermissionEntity getCreate() {
-        return create;
-    }
-
-    public static PermissionEntity getRead() {
-        return read;
-    }
-
-    public static PermissionEntity getUpdate() {
-        return update;
-    }
-
-    public static RoleEntity getAdmin() {
-        return admin;
-    }
-
-    public static PermissionEntity getDelete() {
-        return delete;
-    }
-
-    public static UserEntity getUser() {
-        return user;
     }
 }
