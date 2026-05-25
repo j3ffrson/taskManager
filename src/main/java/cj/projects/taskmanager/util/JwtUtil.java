ad86c97593f8796dd77c45e452160234acea34bd
Jefferson Chaustre <chaustrejefferson@gmail.com>
2026-05-25 17:47:11 -0500

    feat(JwtUtil): create method generateToken for JWT #28

--- src/main/java/cj/projects/taskmanager/util/JwtUtil.java
+++ src/main/java/cj/projects/taskmanager/util/JwtUtil.java
@@ -1,7 +1,16 @@
 package cj.projects.taskmanager.util;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtUtil {
@@ -10,5 +19,22 @@ public class JwtUtil {
     private String secretKey;
     @Value("${spring.security.jwt.user.generator}")
     private String userGenerator;
+
+    private String generateToken(Authentication authentication){
+        Algorithm algorithm= Algorithm.HMAC256(secretKey);
+        String username= authentication.getName();
+        String permmissions= authentication.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
+        return JWT.create()
+                .withIssuer(this.userGenerator)
+                .withSubject(username)
+                .withClaim("permissions", permmissions)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis()+ TimeUnit.HOURS.toMillis(3)))
+                .withJWTId(UUID.randomUUID().toString())
+                .withNotBefore(new Date(System.currentTimeMillis()))
+                .sign(algorithm);
+
+    }
 
 }
