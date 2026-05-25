ac93f141a7c0962010e5faced8ac3cc08d0c080e
Jefferson Chaustre <chaustrejefferson@gmail.com>
2026-05-25 18:01:17 -0500

    feat(JwtUtil): add methods to extract username and other attributes from JWT #28

--- src/main/java/cj/projects/taskmanager/util/JwtUtil.java
+++ src/main/java/cj/projects/taskmanager/util/JwtUtil.java
@@ -3,6 +3,7 @@ package cj.projects.taskmanager.util;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -50,6 +52,15 @@ public class JwtUtil {
         }catch (JWTVerificationException e){
             throw new JWTVerificationException(e.getMessage());
         }
+    }
+    public String getUsernameFromToken(DecodedJWT decodedJWT){
+        return decodedJWT.getSubject();
+    }
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName){
+        return decodedJWT.getClaim(claimName);
+    }
+    public Map<String,Claim> getClaims(DecodedJWT decodedJWT){
+        return decodedJWT.getClaims();
     }
 
 }
