d3949906c4fee57df1921c9d4c92b01b1d471ba2
Jefferson Chaustre <chaustrejefferson@gmail.com>
2026-05-25 17:33:37 -0500

    feat(JwtUtil): add secretKey and userGenerator

--- src/main/java/cj/projects/taskmanager/util/JwtUtil.java
+++ src/main/java/cj/projects/taskmanager/util/JwtUtil.java
@@ -1,4 +1,14 @@
 package cj.projects.taskmanager.util;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
+
+    @Value("${spring.security.jwt.private.secret.key}")
+    private String secretKey;
+    @Value("${spring.security.jwt.user.generator}")
+    private String userGenerator;
+
 }
