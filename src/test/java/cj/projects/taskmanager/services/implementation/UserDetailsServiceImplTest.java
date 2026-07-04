e0326a08958adaa8f910651ec9fc6979090a72e2
Jefferson Chaustre <chaustrejefferson@gmail.com>
2026-07-04 11:55:15 -0500

    test: make method test for login user

--- src/test/java/cj/projects/taskmanager/services/implementation/UserDetailsServiceImplTest.java
+++ src/test/java/cj/projects/taskmanager/services/implementation/UserDetailsServiceImplTest.java
@@ -6,11 +6,15 @@ import cj.projects.taskmanager.persistence.entities.RoleEntity;
 import cj.projects.taskmanager.persistence.entities.UserEntity;
 import cj.projects.taskmanager.persistence.repositories.RoleRepository;
 import cj.projects.taskmanager.persistence.repositories.UserRepository;
+import cj.projects.taskmanager.services.dto.request.AuthLoginRequest;
+import cj.projects.taskmanager.services.dto.response.AuthResponse;
+import cj.projects.taskmanager.util.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,6 +24,8 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,6 +37,8 @@ class UserDetailsServiceImplTest {
     private RoleRepository roleRepository;
     @Mock
     private PasswordEncoder passwordEncoder;
+    @Mock
+    private JwtUtil jwtUtil;
 
     @InjectMocks
     private UserDetailsServiceImpl userDetailsService;
@@ -63,6 +71,30 @@ class UserDetailsServiceImplTest {
     }
     @Test
     void loginUser() {
+
+        String username= "jeffer";
+        UserEntity userTest= UserDataProvider.getUser();
+        RoleEntity role= RoleDataProvider.roleAdmin();
+        userTest.setRoles(Set.of(role));
+
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(userTest));
+        when(passwordEncoder.matches("milluh123",userTest.getPassword())).thenReturn(true);
+        when(jwtUtil.generateToken(any(Authentication.class))).thenReturn("fake-token");
+
+        AuthLoginRequest authLoginRequest = new AuthLoginRequest("jeffer","milluh123");
+
+        AuthResponse authResponse = userDetailsService.loginUser(authLoginRequest);
+        assertThat(authResponse).isNotNull();
+        assertThat(authResponse.username()).isEqualTo(authLoginRequest.username());
+        assertThat(authResponse.JWT()).isEqualTo("fake-token");
+        assertThat(authResponse.status()).isTrue();
+
+        verify(userRepository).findByUsername(username);
+        verify(passwordEncoder).matches("milluh123",userTest.getPassword());
+        verify(jwtUtil).generateToken(any(Authentication.class));
+
+
+
     }
 
     @Test
