7e9624a79ea32bf692d59ab21fc210c78fe9731d
Jefferson Chaustre <chaustrejefferson@gmail.com>
2026-07-04 11:15:12 -0500

    styile: moved service from oauth2 into the service package

--- src/main/java/cj/projects/taskmanager/services/CustomOauth2UserService.java
+++ src/main/java/cj/projects/taskmanager/services/CustomOauth2UserService.java
@@ -1,10 +1,10 @@
-package cj.projects.taskmanager.configurations.oauth2;
+package cj.projects.taskmanager.services;
 
+import cj.projects.taskmanager.configurations.oauth2.CustomOauth2User;
 import cj.projects.taskmanager.persistence.entities.UserEntity;
 import cj.projects.taskmanager.persistence.repositories.UserRepository;
 import cj.projects.taskmanager.services.implementation.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
--- src/main/java/cj/projects/taskmanager/services/CustomOidcUserService.java
+++ src/main/java/cj/projects/taskmanager/services/CustomOidcUserService.java
@@ -1,10 +1,10 @@
-package cj.projects.taskmanager.configurations.oauth2;
+package cj.projects.taskmanager.services;
 
+import cj.projects.taskmanager.configurations.oauth2.CustomOidcUser;
 import cj.projects.taskmanager.persistence.entities.UserEntity;
 import cj.projects.taskmanager.persistence.repositories.UserRepository;
 import cj.projects.taskmanager.services.implementation.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
