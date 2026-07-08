5af7259bd79d86a182f8dca0b44c573963adeec6
Jefferson Chaustre <37542296+j3ffrson@users.noreply.github.com>
2026-07-08 11:11:48 -0500

    Fix: date range filter and update security configuration (#72)
    * fix: fixed filter task for date range with period time endpoint with params value
    
    * chore: add preauthorize annotations and fix security configuration

--- src/main/java/cj/projects/taskmanager/controller/TaskController.java
+++ src/main/java/cj/projects/taskmanager/controller/TaskController.java
@@ -1,7 +1,6 @@
 package cj.projects.taskmanager.controller;
 
 import cj.projects.taskmanager.services.TaskService;
-import cj.projects.taskmanager.services.dto.request.TaskDateBetweenFilterRequest;
 import cj.projects.taskmanager.services.dto.request.TaskRequest;
 import cj.projects.taskmanager.services.dto.response.TaskDto;
 import jakarta.validation.Valid;
@@ -11,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -25,6 +25,7 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping()
+    @PreAuthorize("hasAuthority('READ')")
     ResponseEntity<Page<TaskDto>> getAllTask(@RequestParam(name = "page",defaultValue = "0") int pageNumber,
                                              @RequestParam(name = "size",defaultValue = "3") int pageSize){
 
@@ -34,6 +35,7 @@ public class TaskController {
     }
 
     @GetMapping("/status/{status}")
+    @PreAuthorize("hasAuthority('READ')")
     ResponseEntity<Page<TaskDto>> getAllTaskByStatus(@RequestParam(name = "page",defaultValue = "0") int pageNumber,
                                                      @RequestParam(name = "size",defaultValue = "3") int pageSize,
                                                      @PathVariable String status){
@@ -44,6 +46,7 @@ public class TaskController {
     }
 
     @GetMapping("/author")
+    @PreAuthorize("hasAuthority('READ')")
     ResponseEntity<Page<TaskDto>> getAllTaskByAuthor(@RequestParam(name = "page",defaultValue = "0") int pageNumber,
                                                      @RequestParam(name = "size",defaultValue = "3") int pageSize){
 
@@ -52,36 +55,40 @@ public class TaskController {
 
     }
 
-    @PostMapping("/period/date")
-    ResponseEntity<Page<TaskDto>> getAllTaskByPeriodTime(@RequestParam(name = "page",defaultValue = "0") int pageNumber,
-                                                     @RequestParam(name = "size",defaultValue = "3") int pageSize,
-                                                         @RequestBody @Valid TaskDateBetweenFilterRequest taskDateBetweenFilterRequest){
+    @GetMapping("/period/date")
+    @PreAuthorize("hasAuthority('READ')")
+    ResponseEntity<Page<TaskDto>> getAllTaskByPeriodTime(@RequestParam(name = "page",defaultValue = "0") int pageNumber, @RequestParam(name = "size",defaultValue = "3") int pageSize,
+                                                         @RequestParam(name = "after") String createAdDateAfter, @RequestParam(name = "before") String createAdDateBefore){
 
         Pageable pageable= PageRequest.of(pageNumber,pageSize);
         DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate dateStart= LocalDate.parse(taskDateBetweenFilterRequest.createAdDateAfter(),formatDate);
-        LocalDate dateEnd= LocalDate.parse(taskDateBetweenFilterRequest.createAdDateBefore(),formatDate);
+        LocalDate dateStart= LocalDate.parse(createAdDateAfter,formatDate);
+        LocalDate dateEnd= LocalDate.parse(createAdDateBefore,formatDate);
 
         return new ResponseEntity<>(taskService.findAllTaskByCreateAdBetween(dateStart,dateEnd,pageable), HttpStatus.OK);
 
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('READ')")
     ResponseEntity<TaskDto> getTaskById(@PathVariable String id){
         return new ResponseEntity<>(taskService.findTaskById(UUID.fromString(id)), HttpStatus.OK);
     }
 
     @PostMapping("/new")
+    @PreAuthorize("hasAuthority('CREATE')")
     ResponseEntity<TaskDto> createNewTask(@RequestBody @Valid TaskRequest taskRequest){
         return new ResponseEntity<>(taskService.createNewTask(taskRequest),HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
+    @PreAuthorize("hasAuthority('UPDATE')")
     ResponseEntity<TaskDto> updateTask(@RequestBody @Valid TaskRequest taskRequest, @PathVariable String id) {
         return new ResponseEntity<>(taskService.updateNewTask(taskRequest,UUID.fromString(id)),HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('DELETE')")
     ResponseEntity<Void> deleteTask(@PathVariable String id){
         taskService.deleteTaskById(UUID.fromString(id));
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
